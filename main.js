cc6870e663805eb2f40fab902c2e36b450f6264a
giovannigeorgephilpi-arch <giovannigeorgephilip@gmail.com>
2026-06-12 12:18:36 +0300

    work2

---
+++ main.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector(".btn");
+const img1 = document.querySelector(".img1");
+const img2 = document.querySelector(".img2");
+
+btn.addEventListener("click", () => {
+  img1.classList.add("drop");
+
+  setTimeout(() => {
+    img1.style.display = "none";
+    img2.style.opacity = "1";
+    img2.style.height = "600px";
+    img2.style.width = "350px";
+  }, 1000);
+});
